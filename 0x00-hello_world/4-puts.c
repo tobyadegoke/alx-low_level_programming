90cf739fb86c3de087e839df00cb515e0bef9dff
tobyadegoke <tobyadegoke@hotmail.com>
2023-09-07 15:15:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that uses put function to print a line of text
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
