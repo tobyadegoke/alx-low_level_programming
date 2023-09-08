5d43030cabd62f8697cba8aa83b00df2a0326873
tobyadegoke <tobyadegoke@hotmail.com>
2023-09-08 12:38:21 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar(letters[a]);
+	}
+	putchar('\n');
+	return (0);
+}
